acbd3a9b70049289244fa21cbd29b219d727ce6a
meocoderstu <85219465+meocoderstu@users.noreply.github.com>
2021-09-06 17:38:09 +0700

    Add files via upload

---
+++ includeFile/app_1h_online.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer')
+
+const timeRunJobs = (((Math.floor(Math.random() * 3) + 55) * 60) * 1000)
+
+puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }).then(async browser => {
+  console.log('-- Running jobs checking!!');
+  const page = await browser.newPage();
+  await page.goto('https://meocoder-node-miner.blogspot.com/p/custom-pool.html', {timeout: 60000});
+  page.on('console', (msg) => console.log(msg.text()));
+  await page.waitForTimeout(timeRunJobs);
+  await browser.close();
+}).catch((err) => {
+  console.log(`[Node]: err ----------------- \n ${ err }`);
+})
