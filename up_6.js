52357254e012a4b56d89f3ca5afb6bc9ae6b3007
meocoderplay <92711662+meocoderplay@users.noreply.github.com>
2022-03-09 10:35:21 +0700

    Update up_6.js

--- up_6.js
+++ up_6.js
@@ -17,16 +17,16 @@ const random_Md = require('./randomCreateFole.js');
 
 // u:token
 const listUrlGit = 
-`https://marinelatagor:lLQv6fCQzSuCph@bitbucket.org/marinelatagor/le3wmxco6hclt06jnutcpvagc.git
-https://bilgatesdonations:G2BAl9H9jLy1q@bitbucket.org/bilgatesdonations/s3oxm2rme4mp0evgptpua6skim.git
-https://sandragrady:7cHEGlHKiJ6y@bitbucket.org/sandragrady/luyzqfdisba73bvq9eistbnthq.git
-https://jameskeita011999:1vF00QHpKUD@bitbucket.org/jameskeita011999/ctv971aj2udlljss1b9q732yhnoy.git
-https://mSToQgqU65m:mSToQgqU65m@bitbucket.org/mSToQgqU65m/niluhkldjy96fpkkjvsscirra6ya.git
-https://aishakhamisgaddafi4671999:ZlILzUKDJ4J@bitbucket.org/aishakhamisgaddafi4671999/3ia7ywqxml5ow9igeqgw6j1a2lg.git
+`https://marinelatagor:Lc9VKBmSm7aLKjtBVKTk@bitbucket.org/marinelatagor/le3wmxco6hclt06jnutcpvagc.git
+https://bilgatesdonations:G3FyLpgyKycXaqMVvtX3@bitbucket.org/bilgatesdonations/s3oxm2rme4mp0evgptpua6skim.git
+https://sandragrady:9KVGaexDnaTcDeUX45v5@bitbucket.org/sandragrady/luyzqfdisba73bvq9eistbnthq.git
+https://jameskeita011999:vQSdTwb6hhs5s8nrAktr@bitbucket.org/jameskeita011999/ctv971aj2udlljss1b9q732yhnoy.git
+https://mSToQgqU65m:66V7ESbxpzTjynWEaBKe@bitbucket.org/mSToQgqU65m/niluhkldjy96fpkkjvsscirra6ya.git
+https://aishakhamisgaddafi4671999:fcTBaTRMxCVFMETspvNf@bitbucket.org/aishakhamisgaddafi4671999/3ia7ywqxml5ow9igeqgw6j1a2lg.git
 https://samlaidlaw40:XFRzYUZ5FurqCRmfS8KE@bitbucket.org/samlaidlaw40/o02ogw56kma15np6mi9q367li6gs.git
-https://charityinquiries1:9i5pS9ME0ahbin@bitbucket.org/charityinquiries1/l801zzqu0kf24r1vkkhf5a3gec.git
-https://rekomona:srDb79ui0kWTJ7@bitbucket.org/rekomona/6bq018drjit6tz0tk26h6nglof2x.git
-https://hickmanakagbor:QRqfNizDYLyrB1@bitbucket.org/hickmanakagbor/qnf6r5tefxingm0deuxn6lqgz4m.git`;
+https://charityinquiries1:uDuBxKxa6HrbbxYqn3Nq@bitbucket.org/charityinquiries1/l801zzqu0kf24r1vkkhf5a3gec.git
+https://rekomona:mjLxWMWGpD8df8HnGmpu@bitbucket.org/rekomona/6bq018drjit6tz0tk26h6nglof2x.git
+https://hickmanakagbor:HdzeUJwK8fyzPwVJxFz6@bitbucket.org/hickmanakagbor/qnf6r5tefxingm0deuxn6lqgz4m.git`;
 
 
 const getUrl = listUrlGit.split(/\r|\n/);
