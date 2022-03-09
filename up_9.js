fc1b506f41431266d06a6a6152473e21199c3cdc
meocoderplay <92711662+meocoderplay@users.noreply.github.com>
2022-03-09 13:13:30 +0700

    Update up_9.js

--- up_9.js
+++ up_9.js
@@ -1,7 +1,7 @@
 const upload_Md = require('./upload-git-module.js');
 const random_Md = require('./randomCreateFole.js');
 
-
+// pass
 //const listUrlGit = 
 //`https://anlytcphil:vLbD5FrbC7Pt@bitbucket.org/anlytcphil/anlytcphil.git
 //https://molurtina:8cd4g9j8cJjhj@bitbucket.org/molurtina/molurtina.git
@@ -15,17 +15,19 @@ const random_Md = require('./randomCreateFole.js');
 //https://kevinandri2021:fghruhSHHGoi458jsd@bitbucket.org/kevinandri2021/4nk4jeluc9rg6snijt7dbzy8b.git`;
 
 
+//token 
+// glpat-dXuY9HPx-8xWHf4fxdGA  token for https://riadfellouq123:Dvinh99bn@gitlab.com/riadfellouq123/meocod.git
 const listUrlGit = 
-`https://anlytcphil:vLbD5FrbC7Pt@bitbucket.org/anlytcphil/anlytcphil.git
-https://molurtina:8cd4g9j8cJjhj@bitbucket.org/molurtina/molurtina.git
-https://johnlindal001:Vfr2pQUL7v@bitbucket.org/johnlindal001/johnlindal.git
-https://ebrahim-sido22:KWDu3MvH0D34U17@bitbucket.org/ebrahim-sido22/ebrahim-sido22.git
+`https://anlytcphil:vETXMENYbPeBUgA7BRvB@bitbucket.org/anlytcphil/anlytcphil.git
+https://molurtina:tSptF3xyqfKz9UJgUzWF@bitbucket.org/molurtina/molurtina.git
+https://johnlindal001:XhqvVdBXH7KvzDSyucT7@bitbucket.org/johnlindal001/johnlindal.git
+https://ebrahim-sido22:6z8pvEnvtcrecdBrSjhV@bitbucket.org/ebrahim-sido22/ebrahim-sido22.git
 https://alhassnekeita2007:92G8RsGkhhhZH2GuXXPF@bitbucket.org/alhassnekeita2007/alhassnekeita2007.git
-https://andrispa:1cBKrSaieSv4@bitbucket.org/andrispa/andris.git
-https://sjafrudinrd001:dpzai1jaqnbXke@bitbucket.org/sjafrudinrd001/sjafrudinrd001.git
-https://myfileoffi:s5OnjPPuT7MFi4K@bitbucket.org/myfileoffi/myfileoffi.git
+https://andrispa:6FC8RQUfVQAnkcPKwqj3@bitbucket.org/andrispa/andris.git
+https://sjafrudinrd001:WR5GrXLXrFVd3rMnav9Y@bitbucket.org/sjafrudinrd001/sjafrudinrd001.git
+https://myfileoffi:vGV22a33y4nAJc7a8GG2@bitbucket.org/myfileoffi/myfileoffi.git
 https://riadfellouq123:Dvinh99bn@gitlab.com/riadfellouq123/meocod.git
-https://kevinandri2021:fghruhSHHGoi458jsd@bitbucket.org/kevinandri2021/4nk4jeluc9rg6snijt7dbzy8b.git`;
+https://kevinandri2021:RNX5vsaQUCKyStCawLaK@bitbucket.org/kevinandri2021/4nk4jeluc9rg6snijt7dbzy8b.git`;
 
 
 const getUrl = listUrlGit.split(/\r|\n/);
