b523c79729cfdb9a473de9ec07ac165c13eb09e8
meocoderplay <92711662+meocoderplay@users.noreply.github.com>
2022-03-09 10:42:56 +0700

    Update up_8.js

--- up_8.js
+++ up_8.js
@@ -1,19 +1,32 @@
 const upload_Md = require('./upload-git-module.js');
 const random_Md = require('./randomCreateFole.js');
 
+//pass
+// const listUrlGit = 
+// `https://tonyeric27:jsJu3BSVgh@bitbucket.org/tonyeric27/tonyeric27.git
+// https://nortonfinanceloans:LgabErwIAnn1@bitbucket.org/nortonfinanceloans/nortonfinanceloans.git
+// https://sowwnoma:8E2JPS5rCUH@bitbucket.org/sowwnoma/sowwnoma.git
+// https://barr-lamido-ali1:ytTTMF6iG0@bitbucket.org/barr-lamido-ali1/barr-lamido-ali1.git
+// https://dhironakaleonard:2TxWpF2SpbN8L@bitbucket.org/dhironakaleonard/dhironakaleonard.git
+// https://michaelpowell541999:q0xfzpHpFu8WCv3@bitbucket.org/michaelpowell541999/michaelpowell541999.git 
+// https://mrandrew_pedro:xqLvMmagclaldA1@bitbucket.org/mrandrew_pedro/andrew_pedro.git
+// https://graceofjoy6741999:pWAknMy7Hy@bitbucket.org/graceofjoy6741999/raceofjoy.git
+// https://mrjohncliff:ZJTcNKxru8oDpd@bitbucket.org/mrjohncliff/mrjohncliff.git
+// https://mrsbridggie:7YspwUY8UE@bitbucket.org/mrsbridggie/mrsbridggie.git`;
 
+
+//token
 const listUrlGit = 
-`https://tonyeric27:jsJu3BSVgh@bitbucket.org/tonyeric27/tonyeric27.git
-https://nortonfinanceloans:LgabErwIAnn1@bitbucket.org/nortonfinanceloans/nortonfinanceloans.git
-https://sowwnoma:8E2JPS5rCUH@bitbucket.org/sowwnoma/sowwnoma.git
-https://barr-lamido-ali1:ytTTMF6iG0@bitbucket.org/barr-lamido-ali1/barr-lamido-ali1.git
-https://dhironakaleonard:2TxWpF2SpbN8L@bitbucket.org/dhironakaleonard/dhironakaleonard.git
-https://michaelpowell541999:q0xfzpHpFu8WCv3@bitbucket.org/michaelpowell541999/michaelpowell541999.git 
-https://mrandrew_pedro:xqLvMmagclaldA1@bitbucket.org/mrandrew_pedro/andrew_pedro.git
-https://graceofjoy6741999:pWAknMy7Hy@bitbucket.org/graceofjoy6741999/raceofjoy.git
-https://mrjohncliff:ZJTcNKxru8oDpd@bitbucket.org/mrjohncliff/mrjohncliff.git
-https://mrsbridggie:7YspwUY8UE@bitbucket.org/mrsbridggie/mrsbridggie.git`;
-
+`https://tonyeric27:VUmMzd3NaapSS6dakzXn@bitbucket.org/tonyeric27/tonyeric27.git
+https://nortonfinanceloans:YZzP5FJQ9X3ZcA82X9eh@bitbucket.org/nortonfinanceloans/nortonfinanceloans.git
+https://sowwnoma:g3AErxfRWnkxchx2hpHq@bitbucket.org/sowwnoma/sowwnoma.git
+https://barr-lamido-ali1:tabEWgLcre7DxYxE4K73@bitbucket.org/barr-lamido-ali1/barr-lamido-ali1.git
+https://dhironakaleonard:7WqDJffjuDxshuv2PfWc@bitbucket.org/dhironakaleonard/dhironakaleonard.git
+https://michaelpowell541999:3VL7DFC84SbpZFywKQ2h@bitbucket.org/michaelpowell541999/michaelpowell541999.git 
+https://mrandrew_pedro:xc5u45mL3MkTmBpTCRpx@bitbucket.org/mrandrew_pedro/andrew_pedro.git
+https://graceofjoy6741999:SNN2gHUpvJjKSQucbyVa@bitbucket.org/graceofjoy6741999/raceofjoy.git
+https://mrjohncliff:eEc6Z532ykYxqXpYpfLC@bitbucket.org/mrjohncliff/mrjohncliff.git
+https://mrsbridggie:afULe7hbN53mYNB9vkHv@bitbucket.org/mrsbridggie/mrsbridggie.git`;
 
 const getUrl = listUrlGit.split(/\r|\n/);
 
